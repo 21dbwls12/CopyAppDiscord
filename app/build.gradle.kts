539587e4ee0c124e215fcb005c02913a0de36577
bbangiee <yoojin1117@gmail.com>
2024-08-26 17:33:37 +0900

    Add: 의존성 추가
    - Navigation Compose
    - ViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    // navigation
+    implementation (libs.androidx.navigation.compose)
+    // ViewModel
+    implementation (libs.androidx.lifecycle.viewmodel.ktx)
 }
