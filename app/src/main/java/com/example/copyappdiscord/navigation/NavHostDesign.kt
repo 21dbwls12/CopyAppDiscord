6685ddc16add98e793534ecf1784ff3f980e7814
bbangiee <yoojin1117@gmail.com>
2024-09-03 18:57:30 +0900

    Add: NavHost 설계
    - 앱 시작시 가장 처음 나올 화면 추가
      - StartScreen.kt

---
+++ app/src/main/java/com/example/copyappdiscord/navigation/NavHostDesign.kt
@@ -0,0 +1,15 @@
+package com.example.copyappdiscord.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.copyappdiscord.screen.StartScreen
+
+@Composable
+fun NavHostDesign(navController: NavHostController = rememberNavController(), startDestination: String = NavPath.Start.route) {
+    NavHost(navController = navController, startDestination = startDestination) {
+        composable(startDestination) { StartScreen(navController) }
+    }
+}
