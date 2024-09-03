2025aab6c5d637c018d19e95d7eab4a928695c1c
bbangiee <yoojin1117@gmail.com>
2024-09-03 19:00:02 +0900

    Add: 앱 시작시 가장 처음 나타날 화면 파일 생성

---
+++ app/src/main/java/com/example/copyappdiscord/screen/StartScreen.kt
@@ -0,0 +1,9 @@
+package com.example.copyappdiscord.screen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun StartScreen(navController: NavController) {
+
+}
