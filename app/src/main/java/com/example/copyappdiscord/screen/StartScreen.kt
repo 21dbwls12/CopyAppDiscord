3ebd4cd988398ae94e18d975874b081484fea2d4
bbangiee <yoojin1117@gmail.com>
2024-09-25 16:12:04 +0900

    Docs: import 수정

--- app/src/main/java/com/example/copyappdiscord/screen/StartScreen.kt
+++ app/src/main/java/com/example/copyappdiscord/screen/StartScreen.kt
@@ -1,22 +1,16 @@
 package com.example.copyappdiscord.screen
 
-import android.os.Looper
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
 import com.example.copyappdiscord.R
-import com.example.copyappdiscord.navigation.NavPath
-import java.util.logging.Handler
 
 @Composable
 fun StartScreen(navController: NavController) {
