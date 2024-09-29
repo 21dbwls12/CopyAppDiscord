89988145622e80414e602b5fc074dba4ac94b1ca
bbangiee <yoojin1117@gmail.com>
2024-09-29 23:21:27 +0900

    Feat: 시작화면 버튼 구현
    - 가입하기, 로그인 버튼 구현

--- app/src/main/java/com/example/copyappdiscord/screen/StartScreen.kt
+++ app/src/main/java/com/example/copyappdiscord/screen/StartScreen.kt
@@ -4,12 +4,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -18,12 +20,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.copyappdiscord.R
@@ -35,12 +41,13 @@ fun StartScreen(navController: NavController) {
         modifier = Modifier.fillMaxSize()
     ) {
         StartScreenBG()
-        StartScreenContent()
+        StartScreenContentTop()
+        StartScreenContentBottom()
     }
 }
 
 @Composable
-fun StartScreenBG() {
+private fun StartScreenBG() {
     Image(
         painter = painterResource(id = R.drawable.nightsky),
         contentDescription = "Background of start screen",
@@ -50,12 +57,13 @@ fun StartScreenBG() {
 }
 
 @Composable
-fun StartScreenContent() {
+private fun StartScreenContentTop() {
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxWidth()
+            .fillMaxHeight(0.9f)
             .padding(15.dp)
     ) {
         Image(
@@ -63,8 +71,28 @@ fun StartScreenContent() {
             contentDescription = "Discord Icon",
             modifier = Modifier.size(90.dp)
         )
-        Spacer(modifier = Modifier.height(16.dp))
         WelcomeHeadText()
+        WelcomeBodyText(text = "어울리고, 게임하고, 가볍게 대화하세요.")
+        WelcomeBodyText(text = "아래를 탭해 시작해요!")
+    }
+}
+
+@Composable
+private fun StartScreenContentBottom() {
+    Column(
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(1f)
+            .padding(vertical = 30.dp, horizontal = 50.dp)
+    ) {
+        StartButton(text = "가입하기", isSignUp = true) {
+
+        }
+        StartButton(text = "로그인", isSignUp = false) {
+
+        }
     }
 }
 
@@ -76,9 +104,16 @@ private fun WelcomeHeadText() {
             CustomStyledText(text = "DISCORD", style = welcomeHeadDiscordSpanStyle())
             CustomStyledText(text = "에 오신 걸 환영합니다", style = welcomeHeadRestSpanStyle())
         },
-        lineHeight = 38.sp,
         textAlign = TextAlign.Center,
         modifier = Modifier.fillMaxWidth(),
+        style = TextStyle(
+            lineHeight = 2.6.em,
+            platformStyle = PlatformTextStyle(includeFontPadding = false),
+            lineHeightStyle = LineHeightStyle(
+                alignment = LineHeightStyle.Alignment.Center,
+                trim = LineHeightStyle.Trim.Both
+            )
+        )
     )
 }
 
@@ -99,7 +134,7 @@ private fun welcomeHeadRestSpanStyle(): SpanStyle {
         color = Color.White,
         fontSize = 33.sp,
         fontWeight = FontWeight.Bold,
-        letterSpacing = 0.2.sp
+        letterSpacing = 0.2.sp,
     )
 }
 
@@ -111,15 +146,26 @@ private fun AnnotatedString.Builder.CustomStyledText(text: String, style: SpanSt
 }
 
 @Composable
-fun WelcomeBodyText(text: String, fontWeight: Int) {
+private fun WelcomeBodyText(text: String) {
     Text(
         text = text,
         modifier = Modifier.fillMaxWidth(),
         color = Color.White,
-        fontSize = 35.sp,
-        fontWeight = FontWeight(fontWeight),
+        fontSize = 17.sp,
         letterSpacing = 0.2.sp,
         textAlign = TextAlign.Center,
-        lineHeight = 38.sp
+        lineHeight = 16.sp
     )
+}
+
+@Composable
+private fun StartButton(text: String, isSignUp: Boolean, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier.fillMaxWidth().padding(top = 12.dp),
+        shape = RoundedCornerShape(30.dp),
+        colors = ButtonDefaults.buttonColors(if (isSignUp) Color.White else Color(0xFF161CBB))
+    ) {
+        Text(text = text, color = if (isSignUp) Color.Black else Color.White, fontWeight = FontWeight.Bold, fontSize = 15.sp, modifier = Modifier.padding(vertical = 5.dp))
+    }
 }
